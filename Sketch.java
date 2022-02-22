84df72d4000de7bb76eb96ef53b6ee315636f5b9
lucas-pei <lucaspei39@gmail.com>
2022-02-22 09:37:08 -0500

    drew background and part of house

--- Sketch.java
+++ Sketch.java
@@ -23,14 +23,33 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+    // background sky
+    fill(173, 216, 230);
+    rect(0, 0, 400, 275);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // background sun
+    fill(255, 255, 0);
+    ellipse(375, 25, 100, 100);
+
+    //house body
+    fill(168, 96, 50);
+    rect(60, 150, 125, 125);
+
+    //top of the house
+    fill(168, 96, 50);
+    triangle(60, 150, 123, 100, 185, 150);
+
+    //window
+    fill(225, 225, 225);
+    ellipse(150, 190, 30, 30);
+
+    //door
+    fill(145, 61, 7);
+    rect(110, 225, 25, 50);
+
+
   }
   
-  // define other methods down here.
+
 }
